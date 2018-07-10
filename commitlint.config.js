f2be3a8f480facd3219ba9fb0fa978c83bc0096d
Devin Lin <adntin@qq.com>
2018-07-10 15:55:35 +0800

    chore: body and footer support Empty

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    // "scope-empty": [2, "always"],
+    "body-empty": [2, "always"],
+    "footer-empty": [2, "always"],
+    "subject-case": [2, "never", []]
+  }
+};
