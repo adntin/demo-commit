a69271c8deb2fc6eed9df9e70ba444b8727be989
Devin Lin <adntin@qq.com>
2018-07-10 16:07:49 +0800

    chore(rules): change commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,9 +2,9 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     // "scope-empty": [2, "always"],
+    // "scope-enum": [2, "always", ["container", "components"]],
     "body-empty": [2, "always"],
     "footer-empty": [2, "always"],
-    "subject-case": [2, "never", []],
-    "scope-enum": [2, "always", ["container", "components"]]
+    "subject-case": [2, "never", []]
   }
 };
